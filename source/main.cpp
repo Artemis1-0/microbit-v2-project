d6601d2a9a23bfd320f6a70c80a6df8ae906bc09
ArtemisOne <141044353+Artemis1-0@users.noreply.github.com>
2026-03-15 19:36:31 +0000

    Got sound kinda working.

--- source/main.cpp
+++ source/main.cpp
@@ -6,14 +6,27 @@ MicroBit uBit;
 void buttonA(MicroBitEvent e)
 {
     uBit.io.speaker.setAnalogValue( 512 );
-    uBit.io.speaker.setAnalogPeriod( 1000 );
-    uBit.sleep( 1000 );
+    uBit.io.speaker.setAnalogPeriodUs( 1136 );
+    uBit.sleep( 250 );
+    uBit.io.speaker.setAnalogValue( 0 );
+    uBit.sleep( 50 );
+    uBit.io.speaker.setAnalogValue( 512 );
+    uBit.io.speaker.setAnalogPeriodUs( 1136 );
+    uBit.sleep( 250 );
+    uBit.io.speaker.setAnalogValue( 0 );
+    uBit.sleep( 50 );
+    uBit.io.speaker.setAnalogValue( 512 );
+    uBit.io.speaker.setAnalogPeriodUs( 1136 );
+    uBit.sleep( 500 );
     uBit.io.speaker.setAnalogValue( 0 );
 }
 
 int main()
 {
     uBit.init();
+    buttonA(MicroBitEvent());
     uBit.messageBus.listen( MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, buttonA );
+
+    release_fiber();
 }
 
