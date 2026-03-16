917458c7010314d54061280c7105c8c0e84c49a9
ArtemisOne <141044353+Artemis1-0@users.noreply.github.com>
2026-03-16 17:19:46 +0000

    Some more fixes.

--- source/robotBoard.cpp
+++ source/robotBoard.cpp
@@ -5,12 +5,12 @@ KitronikRoboticsBoard::KitronikRoboticsBoard() {
     uint8_t buf[2];
     buf[0] = PRESCALE_REG;
     buf[1] = 0x7D;
-    uBit.i2c.write(chipAddress, buf, false);
+    uBit.i2c.write(chipAddress << 1, buf, 2);
 
     for (int blockReg = 0xFA; blockReg < 0xFE; blockReg++) {
     buf[0] = blockReg;
     buf[1] = 0x00;
-    uBit.i2c.write(chipAddress, buf, false);
+    uBit.i2c.write(chipAddress << 1, buf, 2);
     }
 }
 
@@ -29,7 +29,7 @@ void KitronikRoboticsBoard::servoWrite(int servo, int degrees) {
 
     buf[0] = calcServo;
     buf[1] = int(PWMVal);
-    uBit.i2c.write(chipAddress, buf, false);
+    uBit.i2c.write(chipAddress << 1, buf, 2);
     buf[0] = calcServo + 1;
 
     if (HighByte)
@@ -41,7 +41,7 @@ void KitronikRoboticsBoard::servoWrite(int servo, int degrees) {
         buf[1] = 0x00;
     }
 
-    uBit.i2c.write(chipAddress, buf, false);
+    uBit.i2c.write(chipAddress << 1, buf, 2);
 }
 
 void KitronikRoboticsBoard::motorOn(int motor, Direction direction, int speed) {
@@ -78,12 +78,11 @@ void KitronikRoboticsBoard::motorOn(int motor, Direction direction, int speed) {
         for (int offset = 4; offset < 6; offset++) {
             buf[0] = motorReg + offset;
             buf[1] = 0x00;
-            uBit.i2c.write(chipAddress << 1, buf, false);
+            uBit.i2c.write(chipAddress << 1, buf, 2);
         }
     }
     else if (direction == REVERSE)
     {
-        {
             if (OutputVal > 0xFF)
             {
                 HighByte = true;
@@ -109,26 +108,25 @@ void KitronikRoboticsBoard::motorOn(int motor, Direction direction, int speed) {
             for (int offset2 = 0; offset2 < 2; offset2++) {
                 buf[0] = motorReg + offset2;
                 buf[1] = 0x00;
-                uBit.i2c.write(chipAddress << 1, buf, false);
+                uBit.i2c.write(chipAddress << 1, buf, 2);
             }
         }
     }
-}
 
 void KitronikRoboticsBoard::motorOff(int motor) {
     uint8_t buf[2];
     int motorReg = MOTOR_REG_BASE + (2 * (motor - 1) * REG_OFFSET);
 
-    for (int offset3 = 0; offset3 < 6; offset3++) {
+    for (int offset3 = 0; offset3 < 2; offset3++) {
         buf[0] = motorReg + offset3;
         buf[1] = 0x00;
-        uBit.i2c.write(chipAddress << 1, buf, false);
+        uBit.i2c.write(chipAddress << 1, buf, 2);
     }
 
     for (int offset4 = 4; offset4 < 6; offset4++) {
         buf[0] = motorReg + offset4;
         buf[1] = 0x00;
-        uBit.i2c.write(chipAddress << 1, buf, false);
+        uBit.i2c.write(chipAddress << 1, buf, 2);
     }
 }
 
@@ -137,7 +135,7 @@ void KitronikRoboticsBoard::allOff() {
     int servoOffCount = 0;
     int servoRegCount = 0;
 
-    for (int motors = 1; motors <= 5; motors++) {
+    for (int motors = 1; motors < 5; motors++) {
         motorOff(motors);
     }
 
@@ -146,7 +144,7 @@ void KitronikRoboticsBoard::allOff() {
         for (int offset5 = 0; offset5 < 2; offset5++) {
             buf[0] = SRV_REG_BASE + servoRegCount + offset5;
             buf[1] = 0x00;
-            uBit.i2c.write(chipAddress << 1, buf, false);
+            uBit.i2c.write(chipAddress << 1, buf, 2);
         }
         servoRegCount += 4;
         servoOffCount += 1;
